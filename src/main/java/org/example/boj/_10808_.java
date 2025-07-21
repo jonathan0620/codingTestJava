c4c191d5dc4e107a3e2a30179652db12045817f5
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-21 18:20:32 +0900

    feat: 10808 알파벳세기

---
+++ src/main/java/org/example/boj/_10808_.java
@@ -0,0 +1,24 @@
+package org.example.boj;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _10808_ {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("input.txt"));
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            System.out.print(count[i] + " ");
+        }
+    }
+}
