197e14c55dcab206cdf63fadfaff3508ccdb9bd7
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-21 17:12:01 +0900

    feat: 2744_대소문자 바꾸기 문제

--- src/main/java/org/example/boj/_2744_.java
+++ src/main/java/org/example/boj/_2744_.java
@@ -1,4 +1,16 @@
 package org.example.boj;
 
+import java.util.Scanner;
+
 public class _2744_ {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for (int i=0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if ('A' <= ch && ch <= 'Z')
+                System.out.print((char)('a' + ch - 'A'));
+            else System.out.print((char)('A' + ch - 'a'));
+        }
+    }
 }
