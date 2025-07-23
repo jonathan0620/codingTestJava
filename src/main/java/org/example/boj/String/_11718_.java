ef588502e7da910f6a77ca0d6c69493927a3da2d
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-23 14:54:07 +0900

    feat: 11718 hasNextLine

---
+++ src/main/java/org/example/boj/String/_11718_.java
@@ -0,0 +1,14 @@
+package org.example.boj.String;
+
+import java.util.Scanner;
+
+public class _11718_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            System.out.println(line);
+        }
+    }
+}
