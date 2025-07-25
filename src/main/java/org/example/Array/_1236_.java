f2965c637b4048cc39699140e800f949ca83d234
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-25 11:43:46 +0900

    feat: 1236 성 지키기

--- src/main/java/org/example/Array/_1236_.java
+++ src/main/java/org/example/Array/_1236_.java
@@ -14,33 +14,29 @@ public class _1236_ {
         }
 
         // 1. 각 행/열에 대해 경비원이 있는지 확인한다.
-        int existRowCount = 0;
+        boolean[] existRow = new boolean[N];
+        boolean[] existCol = new boolean[M];
         for (int i = 0; i < N; i++) {
-            boolean exist = false;
             for (int j = 0; j < M; j++){
                 if (map[i][j] == 'X') { // 경비원은 X니까
-                    exist = true;
-                    break;
+                    existRow[i] = true;
+                    existCol[j] = true;
                 }
             }
-            if (exist) existRowCount ++;
-        }
-
-        int existColCount = 0;
-        for (int i = 0; i < M; i++) {
-            boolean exist = false;
-            for (int j = 0; j < N; j++){
-                if (map[i][j] == 'X') {
-                    exist = true;
-                    break;
-                }
-            }
-            if (exist) existColCount++;
-        }
+        } // O( N * M )
 
         // 2. 보호받지 못하는 행/열의 갯수를 구한다.
-        int needRowCount = N - existRowCount;
-        int needColCount = M - existColCount;
+        int needRowCount = N;
+        int needColCount = M;
+
+        // O(N)
+        for (int i = 0; i<N; i++)
+            if (existRow[i]) needRowCount--;
+        // O(M)
+        for (int i = 0; i<M; i++)
+            if (existCol[i]) needColCount--;
+
+        // O(N*M) + O(N*M) + O(N) + O(M) => O(NM)
 
         // 3. 둘 중 큰 값을 출력한다.
         System.out.println(Math.max(needRowCount, needColCount));
