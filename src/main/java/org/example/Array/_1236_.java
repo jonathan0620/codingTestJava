1007a3613a7e152fd7ae3de82741ade996285148
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-25 11:36:38 +0900

    feat: 1236 성 지키기 / 런타임 에러

---
+++ src/main/java/org/example/Array/_1236_.java
@@ -0,0 +1,48 @@
+package org.example.Array;
+
+import java.util.Scanner;
+
+public class _1236_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        char[][] map = new char[N][M]; // 2차원 이상이면 캐릭터 어레이로 받자
+        for (int i = 0; i < N; i++){
+            map[i] = sc.next().toCharArray();
+        }
+
+        // 1. 각 행/열에 대해 경비원이 있는지 확인한다.
+        int existRowCount = 0;
+        for (int i = 0; i < N; i++) {
+            boolean exist = false;
+            for (int j = 0; j < M; j++){
+                if (map[i][j] == 'X') { // 경비원은 X니까
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist) existRowCount ++;
+        }
+
+        int existColCount = 0;
+        for (int i = 0; i < M; i++) {
+            boolean exist = false;
+            for (int j = 0; j < N; j++){
+                if (map[i][j] == 'X') {
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist) existColCount++;
+        }
+
+        // 2. 보호받지 못하는 행/열의 갯수를 구한다.
+        int needRowCount = N - existRowCount;
+        int needColCount = M - existColCount;
+
+        // 3. 둘 중 큰 값을 출력한다.
+        System.out.println(Math.max(needRowCount, needColCount));
+    }
+}
