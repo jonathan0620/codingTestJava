5a6c118377187d85089b1ea95ffd9eb169487598
jonadan <0620jonathan@gmail.com>
2025-07-30 15:19:06 +0900

    feat: 10448 유레카이론

---
+++ src/main/java/org/example/BruteForce/_10448_.java
@@ -0,0 +1,41 @@
+package org.example.BruteForce;
+
+import java.util.Scanner;
+
+public class _10448_ {
+    static boolean[] isEurekaNumber = new boolean[1001];
+
+    public static void preprocess() {
+        // 1. K보다 작은 삼각수를 모두 구한다.
+        int[] triangleNumbers = new int[50]; // 넉넉하게 50
+        int triangleNumberCount = 0;
+        for (int i = 1; ; i++) {
+            int triangleNumber = 1 * (i + 1) / 2;
+            if (triangleNumber >= 1000) break;
+            triangleNumbers[triangleNumberCount++] = triangleNumber;
+        } // 1000보다 작은 삼각수를 모두 구한 것
+        // 2. 구해진 삼각수 세개의 합으로 K를 나타낼 수 있는지 확인한다.
+        boolean[] isSumOfTriangleNumber = new boolean[1000];
+        for (int i = 0; i < triangleNumberCount; i++)
+            for (int j = 0; j < triangleNumberCount; j++) {
+                int sum = triangleNumbers[i] + triangleNumbers[j];
+                if (sum < 1000) isSumOfTriangleNumber[sum] = true;
+            }
+        for (int i = 1; i < 1000;i++) {
+            for (int j = 0; j < triangleNumberCount; j++) {
+                int sum = i + triangleNumbers[j];
+                if (sum <= 1000) isEurekaNumber[sum] = true;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // [3, 1000]
+        // isEurekaNumber[]
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            System.out.println(isEurekaNumber[K] ? "1" : "0");
+        }
+    }
+}
